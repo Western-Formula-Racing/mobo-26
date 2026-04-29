2e73c0c3b326e16ded174e298c3b3fa910622610
yamaan93 <yamaan93@gmail.com>
2026-04-29 17:05:19 -0400

    update charging limit

--- include/config.h
+++ include/config.h
@@ -1,12 +1,12 @@
 #define PRECHARGE_TIMEOUT 7000    // milliseconds
 #define PRECHARGE_RATIO   0.9    // % of pack voltage to complete precharge at
-#define CHARGE_TARGET     410     // voltage to stop charging at
+#define CHARGE_TARGET     415     // voltage to stop charging at
 #define CHARGE_CURRENT    5
 #define DO_BUS_RECOVERY   true
 #define MAX_RECOVERY_ATTEMPTS 10
 
 #define OVERTEMP_THRESHOLD 60
-#define OVERVOLTAGE_THRESHOLD 4.15
+#define OVERVOLTAGE_THRESHOLD 4.20
 #define UNDERVOLTAGE_THRESHOLD 2.7
 #define MAX_CELL_DELTA  0.2
 #define CURRENT_LIMIT 200
